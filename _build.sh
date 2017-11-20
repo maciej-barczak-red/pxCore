d213d9b4ecc4fafcf5fd824b6a0399ac17265650
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2017-11-20 14:02:41 +0100

    add build scripts

---
+++ _build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+mkdir -p build
+pushd build
+cmake \
+  -DSUPPORT_NODE=ON \
+  -DSUPPORT_DUKTAPE=ON \
+  -DPXCORE_WAYLAND_DISPLAY_READ_EVENTS=OFF \
+  -DDISABLE_DEBUG_MODE=ON \
+  -DBUILD_PX_TESTS=ON \
+  -DBUILD_WITH_STATIC_NODE=OFF \
+  -DPREFER_SYSTEM_LIBRARIES=ON \
+  -DBUILD_PXSCENE_STATIC_LIB=ON \
+  -DPXSCENE_TEST_HTTP_CACHE=OFF \
+  -DBUILD_DEBUG_METRICS=ON \
+  -DCMAKE_CXX_FLAGS="-fno-delete-null-pointer-checks -Wno-all -Wno-extra -Wno-address -DMESA_EGL_NO_X11_HEADERS -O0 -g3 -DPXSCENE_DISABLE_WST_DECODER" ..
+time make -j$(nproc) VERBOSE=1 && echo "ok"
+popd
