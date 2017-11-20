853a83496770a09298235346a6bc1c90d0342591
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2017-11-20 14:02:41 +0100

    add build scripts

---
+++ _build-wayland-egl.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+mkdir -p build-wayland-egl
+pushd build-wayland-egl
+cmake \
+  -DBUILD_PX_TESTS=OFF \
+  -DBUILD_WITH_STATIC_NODE=OFF \
+  -DPREFER_SYSTEM_LIBRARIES=ON \
+  -DBUILD_RTREMOTE_LIBS=ON \
+  -DPXCORE_WAYLAND_EGL=ON \
+  -DBUILD_PXSCENE_WAYLAND_EGL=ON \
+  -DBUILD_WITH_GL=ON \
+  -DBUILD_WITH_WAYLAND=ON \
+  -DCMAKE_CXX_FLAGS="-fno-delete-null-pointer-checks -Wno-deprecated-declarations -Wno-address -DMESA_EGL_NO_X11_HEADERS -O0 -g3 -DPXSCENE_DISABLE_WST_DECODER" ..
+time make -j$(nproc) VERBOSE=1
+popd
