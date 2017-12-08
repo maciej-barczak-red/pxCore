28138338699bc5e11897a5d08b81635f63548e27
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2017-12-08 19:15:33 +0100

    add possibility to resolve servicemanager dependencies
    fixes the following lookup error while loading libservicemanager.so.1:
      /usr/lib/libpxscene.so: error: symbol lookup error: undefined symbol: _Z6pxMainiPPc (fatal)

--- src/wayland_egl/pxEventLoopNative.cpp
+++ src/wayland_egl/pxEventLoopNative.cpp
@@ -10,26 +10,33 @@
 
 void pxEventLoop::runOnce()
 {
+#ifndef ENABLE_EGL_GENERIC
   pxWindowNative::runEventLoopOnce();
+#endif
 }
 
 void pxEventLoop::run()
 {
     // For now we delegate off to the x11 pxWindowNative class
+#ifndef ENABLE_EGL_GENERIC
     pxWindowNative::runEventLoop();
+#endif //!ENABLE_DFB_GENERIC
 }
 
 void pxEventLoop::exit()
 {
     // For now we delegate off to the x11 pxWindowNative class
+#ifndef ENABLE_EGL_GENERIC
     pxWindowNative::exitEventLoop();
+#endif //!ENABLE_DFB_GENERIC
 }
 
 
 ///////////////////////////////////////////
 // Entry Point 
-
+#ifndef ENABLE_EGL_GENERIC
 int main(int argc, char** argv)
 {
   return pxMain(argc, argv);
 }
+#endif
