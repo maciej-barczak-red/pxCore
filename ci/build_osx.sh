8e3b3125feed5ed20c71a7dfa21055a4eccb80c0
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2017-12-12 14:22:34 +0100

    turn on verbose messages for cmake build - Mac OS

--- ci/build_osx.sh
+++ ci/build_osx.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
-
-BUILDLOGS=$TRAVIS_BUILD_DIR/logs/build_logs
+#!/bin/sh -x
 
 if [[ ! -z $PX_VERSION ]]
 then
@@ -13,12 +11,7 @@ checkError()
   then
   echo "Build failed with errors !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
   echo "CI failure reason: $3"
-  echo "Cause: $4"
-  echo "Reproduction/How to fix: $5"
   if [ "$2" -eq 1 ]
-  then
-  cat $BUILDLOGS
-  fi
   exit 1;
   fi
 }
@@ -34,32 +27,32 @@ cd  temp
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
 
-echo "***************************** Generating config files ****" >> $BUILDLOGS
+echo "***************************** Generating config files ****"
 if [ "$TRAVIS_EVENT_TYPE" != "cron" ] && [ "$TRAVIS_EVENT_TYPE" != "api" ] ;
 then
-cmake -DBUILD_PX_TESTS=ON -DBUILD_PXSCENE_STATIC_LIB=ON -DBUILD_DEBUG_METRICS=ON .. >>$BUILDLOGS 2>&1;
+cmake -DBUILD_PX_TESTS=ON -DBUILD_PXSCENE_STATIC_LIB=ON -DBUILD_DEBUG_METRICS=ON .. 2>&1
 else
-cmake .. >>$BUILDLOGS 2>&1;
+cmake .. 2>&1
 fi
-checkError $? 0 "cmake config failed" "Config error" "Check the error in $BUILDLOGS"
-echo "***************************** Building pxcore,rtcore,pxscene app,libpxscene,unitttests ****" >> $BUILDLOGS
-cmake --build . -- -j1 >>$BUILDLOGS 2>&1;
-checkError $? 0 "Building either pxcore,rtcore,pxscene app,libpxscene,unitttest failed" "Compilation error" "check the $BUILDLOGS file"
+checkError $? 0 "cmake config failed"
+echo "***************************** Building pxcore,rtcore,pxscene app,libpxscene,unitttests ****"
+cmake --build . -- -j1 VERBOSE=1 2>&1
+checkError $? 0 "Building either pxcore,rtcore,pxscene app,libpxscene,unitttest failed"
 
 else
 
 echo "***************************** Generating config files ****"
-cmake -DBUILD_PX_TESTS=ON -DBUILD_PXSCENE_STATIC_LIB=ON -DBUILD_DEBUG_METRICS=ON .. 1>>$BUILDLOGS;
-checkError $? 1  "cmake config failed" "Config error" "Check the errors displayed in this window"
-echo "***************************** Building pxcore,rtcore,pxscene app,libpxscene,unitttests ****" >> $BUILDLOGS
-cmake --build . -- -j1 1>>$BUILDLOGS;
-checkError $? 1 "Building either pxcore,rtcore,pxscene app,libpxscene,unitttest failed" "Compilation error" "Check the errors displayed in this window"
+cmake -DBUILD_PX_TESTS=ON -DBUILD_PXSCENE_STATIC_LIB=ON -DBUILD_DEBUG_METRICS=ON .. 2>&1
+checkError $? 1  "cmake config failed" "Config error"
+echo "***************************** Building pxcore,rtcore,pxscene app,libpxscene,unitttests ****"
+cmake --build . -- -j1 VERBOSE=1 2>&1
+checkError $? 1 "Building either pxcore,rtcore,pxscene app,libpxscene,unitttest failed"
 fi
 
 cd $TRAVIS_BUILD_DIR
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
-echo "***************************** Building pxscene deploy app ***" >> $BUILDLOGS
+echo "***************************** Building pxscene deploy app ***"
 
 if [ "$TRAVIS_EVENT_TYPE" = "cron" ] || [ "$TRAVIS_EVENT_TYPE" = "api" ] ;
 then
@@ -71,24 +64,24 @@ then
 #make PXVERSION=$PX_VERSION deploy >>$BUILDLOGS 2>&1
 #checkError $? 0 "make command failed for deploy target" "Compilation error" "check the $BUILDLOGS file"
 echo "built with cmake"
-./mkdeploy.sh $PX_VERSION >>$BUILDLOGS 2>&1
+./mkdeploy.sh $PX_VERSION 2>&1
 
 else
 
 if [ "$TRAVIS_EVENT_TYPE" = "cron" ]
 then
 export linenumber=`awk '/CFBundleShortVersionString/{ print NR; exit }' $TRAVIS_BUILD_DIR/examples/pxScene2d/src/macstuff/Info.plist`
-checkError $? 0 "unable to read string CFBundleShortVersionString from Info.plist file" "Parse error" "check whether the Info.plist file in pxscene repo is having CFBundleShortVersionString string or not?"
+checkError $? 0 "unable to read string CFBundleShortVersionString from Info.plist file"
 echo $linenumber
 export PX_VERSION=`sed -n "\`echo $((linenumber+1))\`p" $TRAVIS_BUILD_DIR/examples/pxScene2d/src/macstuff/Info.plist|awk -F '<string>' '{print $2}'|awk -F'</string>' '{print $1}'`
-checkError $? 0 "unable to read version from Info.plist file" "Parse error" "check whether the Info.plist file in pxscene repo is having version details or not?"
+checkError $? 0 "unable to read version from Info.plist file"
 #make PXVERSION=$PX_VERSION deploy >>$BUILDLOGS 2>&1
-./mkdeploy.sh $PX_VERSION >>$BUILDLOGS 2>&1
-checkError $? 0 "make command failed for deploy target" "Compilation error" "check the $BUILDLOGS file"
+./mkdeploy.sh $PX_VERSION 2>&1
+checkError $? 0 "make command failed for deploy target"
 
 else
-echo "Deploy terminated as pxversion environment is not set for api event type ************* " >> $BUILDLOGS
-checkError 1 1 "Deploy terminated as pxversion environment is not set" "PX_VERSION environment variable not set" "Set PX_VERSION environment variable and retrigger"
+echo "Deploy terminated as pxversion environment is not set for api event type ************* "
+checkError 1 1 "Deploy terminated as pxversion environment is not set"
 fi
 
 fi
