9dd2a3a2dc0468f71e596b54ae9364d906be9ab6
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2018-01-11 18:00:00 +0100

    align buildNode.sh with ../build.sh
    Correct soft links creation.

--- examples/pxScene2d/external/libnode-v6.9.0/buildNode.sh
+++ examples/pxScene2d/external/libnode-v6.9.0/buildNode.sh
@@ -1,4 +1,6 @@
 ./configure --shared
-make
-ln -sf libnode.so.48 out/Release/obj.target/libnode.so
-ln -sf libnode.48.dylib out/Release/libnode.dylib
+make -j$(getconf _NPROCESSORS_ONLN)
+ln -sf out/Release/obj.target/libnode.so.48 libnode.so.48
+ln -sf libnode.so.48 libnode.so
+ln -sf out/Release/libnode.48.dylib libnode.48.dylib
+ln -sf libnode.48.dylib libnode.dylib
