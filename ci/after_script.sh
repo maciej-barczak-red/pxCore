0879e6826070f66e568c1e556a22b42dd51ca47c
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2018-01-18 20:53:52 +0100

    keep only build script in script.sh
    move other jobs to after_script.sh

--- ci/after_script.sh
+++ ci/after_script.sh
@@ -10,6 +10,30 @@ if [ "$TRAVIS_EVENT_TYPE" = "cron" ] || [ "$TRAVIS_EVENT_TYPE" = "api" ]; then
     exit 0;
 fi
 
+if [ "$TRAVIS_EVENT_TYPE" = "push" ] || [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
+    if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+        export DISPLAY=:99.0
+        /bin/sh -e /etc/init.d/xvfb start
+        sleep 3
+    fi
+
+    cd $TRAVIS_BUILD_DIR/ci
+
+    /bin/sh -x "./unittests_$TRAVIS_OS_NAME.sh"
+    /bin/sh -x "./execute_$TRAVIS_OS_NAME.sh"
+    /bin/sh -x "./code_coverage_$TRAVIS_OS_NAME.sh"
+fi
+
+if [ "$TRAVIS_EVENT_TYPE" = "cron" ] || [ "$TRAVIS_EVENT_TYPE" = "api" ]; then
+    cp $TRAVIS_BUILD_DIR/examples/pxScene2d/src/deploy/mac/*.dmg $TRAVIS_BUILD_DIR/artifacts/.
+    cp $TRAVIS_BUILD_DIR/examples/pxScene2d/src/deploy/mac/software_update.plist $TRAVIS_BUILD_DIR/artifacts/.
+fi
+
+if [ "$TRAVIS_EVENT_TYPE" = "push" ] || [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
+    codecov --build "$TRAVIS_OS_NAME-$TRAVIS_COMMIT-$TRAVIS_BUILD_NUMBER" -X gcov -f $TRAVIS_BUILD_DIR/tracefile
+    genhtml -o $TRAVIS_BUILD_DIR/logs/codecoverage $TRAVIS_BUILD_DIR/tracefile
+fi
+
 cd $TRAVIS_BUILD_DIR
 
 if [ "$TRAVIS_EVENT_TYPE" = "push" ]; then
@@ -32,7 +56,7 @@ if [ "$TRAVIS_EVENT_TYPE" = "api" ] && [ "$UPDATE_VERSION" = "true" ]; then
     echo $linenumber
     sed -i '.bak' "`echo $((linenumber+1))`s/.*/       <string>$PX_VERSION<\\/string>/g" $TRAVIS_BUILD_DIR/examples/pxScene2d/src/macstuff/Info.plist
     cp $TRAVIS_BUILD_DIR/RELEASE_NOTES $TRAVIS_BUILD_DIR/RELEASE_NOTES_bkp
-    echo "===============================================================\n\nRelease $PX_VERSION - `date +\"%d%b%Y\"`\n\n(github.com/johnrobinsn/pxCore: master - SHA `git log --oneline|head -n 1|awk '{print $1}'`)\n\nKnown Issues:\n\nCommits/Fixes:\n" > $TRAVIS_BUILD_DIR/RELEASE_NOTES 
+    echo "===============================================================\n\nRelease $PX_VERSION - `date +\"%d%b%Y\"`\n\n(github.com/johnrobinsn/pxCore: master - SHA `git log --oneline|head -n 1|awk '{print $1}'`)\n\nKnown Issues:\n\nCommits/Fixes:\n" > $TRAVIS_BUILD_DIR/RELEASE_NOTES
     git log  `git log --grep="Change version for release" --oneline -n 1|awk '{print $1}'`..HEAD --oneline --format=%s --no-merges >> $TRAVIS_BUILD_DIR/RELEASE_NOTES
     echo "\n\n" >> $TRAVIS_BUILD_DIR/RELEASE_NOTES
     cat $TRAVIS_BUILD_DIR/RELEASE_NOTES_bkp >> $TRAVIS_BUILD_DIR/RELEASE_NOTES
--- ci/script.sh
+++ ci/script.sh
@@ -21,30 +21,6 @@ else
     export PREFER_SYSTEM_LIBRARIES=OFF
 fi
 
-if [ "$TRAVIS_EVENT_TYPE" = "push" ] || [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
-    /bin/sh -x "./build_$TRAVIS_OS_NAME.sh"
-
-    if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-        export DISPLAY=:99.0
-        /bin/sh -e /etc/init.d/xvfb start
-        sleep 3
-    fi
-
-    /bin/sh -x "./unittests_$TRAVIS_OS_NAME.sh"
-    /bin/sh -x "./execute_$TRAVIS_OS_NAME.sh"
-    /bin/sh -x "./code_coverage_$TRAVIS_OS_NAME.sh"
-else
-    /bin/sh -x "./build_$TRAVIS_OS_NAME.sh"
-fi
-
-if [ "$TRAVIS_EVENT_TYPE" = "cron" ] || [ "$TRAVIS_EVENT_TYPE" = "api" ]; then
-    cp $TRAVIS_BUILD_DIR/examples/pxScene2d/src/deploy/mac/*.dmg $TRAVIS_BUILD_DIR/artifacts/.
-    cp $TRAVIS_BUILD_DIR/examples/pxScene2d/src/deploy/mac/software_update.plist $TRAVIS_BUILD_DIR/artifacts/.
-fi
-
-if [ "$TRAVIS_EVENT_TYPE" = "push" ] || [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
-    codecov --build "$TRAVIS_OS_NAME-$TRAVIS_COMMIT-$TRAVIS_BUILD_NUMBER" -X gcov -f $TRAVIS_BUILD_DIR/tracefile
-    genhtml -o $TRAVIS_BUILD_DIR/logs/codecoverage $TRAVIS_BUILD_DIR/tracefile
-fi
+/bin/sh -x "./build_$TRAVIS_OS_NAME.sh"
 
 exit 0
